3bcf1f8d77f5da7fc7ca45f6f831ffcddcfa758d
niklas-hjelm <niklas.hjelm@iths.se>
2023-10-23 08:52:17 +0200

    Add project files.

---
+++ MovieDataAccess/Data/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieDataAccess.Data;
+
+public record Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+}
